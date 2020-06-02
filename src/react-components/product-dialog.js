0ffe0de336cc65b8eaef1e281967c9b6241ada27
Hironori <56616438+h-nakae@users.noreply.github.com>
2020-06-03 04:08:46 +0900

    add shopping cart to product dialog

--- src/react-components/product-dialog.js
+++ src/react-components/product-dialog.js
@@ -17,7 +17,6 @@ export default class ProductDialog extends Component {
     onSale: PropTypes.bool,
     name: PropTypes.string,
     info: PropTypes.string,
-    photoPath: PropTypes.string,
     price: PropTypes.number,
     genre: PropTypes.string,
     review: PropTypes.number,
@@ -32,6 +31,36 @@ export default class ProductDialog extends Component {
   cartClicked = link => {
     this.setState({ cartButtonActive: false });
   };
+
+  componentDidMount() {
+    // const {async} = this.props;
+    // const script = document.createElement("script");
+
+    // script.src = 'https://digi-rooms.shop-pro.jp/?mode=cartjs&pid=151476901&style=cloth_blue&name=n&img=y&expl=y&stock=y&price=y&optview=n&inq=y&sk=y';
+    // script.type = 'text/javascript';
+    // script.charset ='euc-jp';
+    // script.async = async || false;
+
+    // document.getElementById('colorScript').replaceWith(script)
+     // 1. create iframe to load a library
+     var iframe = document.createElement('iframe');
+     iframe.style.width = '600px';
+     iframe.style.height = '800px';
+     iframe.style.backgroundColor = "#FFFFFF";
+     var head = document.getElementById('colorScript');
+     head.appendChild(iframe);
+
+    // 2. some functions after loading a library 
+    iframe.onload = function() {
+    }
+
+    // 3. write sctipt tag in iframe
+    var html = '<body><script src="https://digi-rooms.shop-pro.jp/?mode=cartjs&pid=151476901&style=cloth_blue&name=n&img=y&expl=y&stock=y&price=y&optview=n&inq=y&sk=y"><\/script><\/body>';
+    var iframeDocument = iframe.contentWindow.document;
+    iframeDocument.open();
+    iframeDocument.write(html);
+    iframeDocument.close();
+  }
 
   render(){
     let proStatus = "";
@@ -52,25 +81,7 @@ export default class ProductDialog extends Component {
         <div>
           <FormattedMessage id={`product.name${this.props.name ? "_modal" : ""}`} /> <span id="product_dialog_product_name">{this.props.name};</span>
         </div>
-        <div>
-            <img id="product_dialog_product_pic" src={this.props.photoPath} alt="" title="" />
-        </div>
-        <div>
-          <FormattedMessage id={`product.info${this.props.info ? "_modal" : ""}`} /> <span id="product_dialog_product_info">{this.props.info};</span>
-        </div>
-        <div>
-          <FormattedMessage id={`product.price${this.props.price ? "_modal" : ""}`} /> <span id="product_dialog_product_price">{this.props.price + 'yen'};</span>
-        </div>
-        <div>
-          <FormattedMessage id={`product.status${this.props.productStatus ? "_modal" : ""}`} /><span id="product_dialog_product_status">{proStatus};</span>
-        </div>
-        <div className={styles.buttons}>
-          <WithHoverSound>
-            <button className={styles.linkButton} onClick={this.cartClicked.bind(this)}>
-    <span>{this.state.cartButtonActive ? <FormattedMessage id={`product.take_it_in_cart`} /> : <FormattedMessage id={`product.taken_in_cart`} /> } </span>
-            </button>
-          </WithHoverSound>
-        </div>
+        <div id='colorScript'/>
         <WithHoverSound>
           <button className={styles.close} onClick={() => this.props.onClose()}>
             <i>
